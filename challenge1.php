d6fc00b11333af80d64c9b087296b2ff65f561bf
AmyXandra <amyabafor013@gmail.com>
2018-04-09 15:20:38 +0100

    push html page

---
+++ challenge1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Challenge1</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body style="background-color:#704BAB; text-align:center; height:100vh; padding:30px; font-family: 'Roboto', sans-serif;">
+<div style="margin-top:80px;">
+<br><br>
+<?php
+echo "<h1 style='color:white;font-size:64px; font-weight:bold; '>" . date("h:i"). "</h1>";
+?>
+<div style="margin:auto; width:30%;" >
+<div style="text-align:left;">
+<h5 style="font-weight:400; font-size:16px;">Hello, Good evening, I'm happy to join this team</h5>
+
+<h4 style="font-weight:bold; font-size:18px;">Challenge1:</h4>
+<h5 style="font-weight:400; font-size:15px;">Set up a page in Figma</h5>
+<h5 style="font-weight:bold; font-size:15px;">DONE!</h5>
+</div>
+</div>
+</div>
+</body>
+</html>
